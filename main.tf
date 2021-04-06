84ef5d687e5089a4de8182a71cecf864326cc2bd
EbisonJ <ebison14@gmail.com>
2021-04-05 23:48:56 -0700

    Initial Commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+
+resource "aws_instance" "myec2" {
+  ami = var.amiid
+  instance_type = "t2.micro"
+  count = var.inst_count
+  tags = {
+    "Name" = var.tag[count.index]
+  }
+}
+
+
+
+output "aws_instanceip"  {
+  value = aws_instance.myec2[*].public_ip
+}
+
+output "aws_private_ip" {
+  value = aws_instance.myec2[*].private_ip
+}
